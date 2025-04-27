bf1b8c39f77c2818c9f1788fe3b65b74996dabaf
helebpet <phelebrantova@dons.usfca.edu>
2025-04-26 23:46:54 -0700

    c

--- js/page3.js
+++ js/page3.js
@@ -1,14 +1,17 @@
 let bgImage;
 let cursorDiv;
 let canvasReady = false;
-let currentLine = 0;
-let lineProgress = 0;
 let timeX, timeY;
 let timeSpeedX = 2;
 let timeSpeedY = 1.5;
 let quoteX, quoteY;
 let targetX, targetY;
 let easing = 0.05;
+
+let sunX, sunY;
+let sunTargetY;
+let sunRadius;
+let sunColor = '#EF5C26';
 
 const quoteText = [
   "Some call it EUPHORIA,",
@@ -18,11 +21,11 @@ const quoteText = [
   "state of consciousness."
 ];
 
+let imagesLoaded = 0;
+
 function preload() {
   bgImage = loadImage('img/page3.jpg', imageLoaded, loadError);
 }
-
-let imagesLoaded = 0;
 
 function imageLoaded() {
   imagesLoaded++;
@@ -48,7 +51,15 @@ function setup() {
   timeY = height / 4;
 
   quoteX = width / 2;
-  quoteY = height / 2; // start at middle vertically
+  quoteY = height / 2;
+
+  sunX = width / 2; // Center the sun horizontally
+  sunY = height / 2; // Center the sun vertically
+
+  sunTargetY = sunY;
+
+  // Set sunRadius proportional to screen size (larger than before)
+  updateSunSize();
 }
 
 function draw() {
@@ -59,11 +70,19 @@ function draw() {
     cnv.style('display', 'block');
   }
 
-  // Background image
+  drawBackground();
+  drawGradientLines(); // Change to the gradient line drawing
+  updateSun();
+  drawSun();
+  drawText();
+  updateCursor();
+}
+
+function drawBackground() {
   let aspectRatio = bgImage.width / bgImage.height;
   let canvasRatio = width / height;
   let drawWidth, drawHeight;
-  
+
   if (canvasRatio > aspectRatio) {
     drawWidth = width;
     drawHeight = width / aspectRatio;
@@ -71,49 +90,65 @@ function draw() {
     drawHeight = height;
     drawWidth = height * aspectRatio;
   }
-  
+
   let offsetX = (width - drawWidth) / 2;
   let offsetY = (height - drawHeight) / 2;
-  
-  image(bgImage, offsetX, offsetY, drawWidth, drawHeight);
 
-  // Animated black lines
+  image(bgImage, offsetX, offsetY, drawWidth, drawHeight);
+}
+
+function drawGradientLines() {
+  // Calculate gradient color based on mouseX position
+  let startColor = color('#EF5C26');  // Orange color
+  let endColor = color('#D7DA1B');    // Yellow color
+  let interColor = lerpColor(startColor, endColor, map(mouseX, 0, width, 0, 1));  // Interpolate between the two colors
+
+  stroke(interColor);
+  strokeWeight(1);
+
+  // Draw static lines across the screen
+  for (let y = 0; y < height; y += 10) {
+    line(0, y, width, y);  // Static horizontal lines
+  }
+}
+
+function updateSun() {
+  if (mouseX > width * 0.7) {
+    sunTargetY = -sunRadius;
+  } else {
+    sunTargetY = height + sunRadius;
+  }
+  sunY += (sunTargetY - sunY) * easing;
+}
+
+function drawSun() {
   stroke(0);
   strokeWeight(1);
-  for (let y = 0; y < currentLine * 10; y += 10) {
-    line(0, y, width, y);
-  }
-  
-  let y = currentLine * 10;
-  if (y < height) {
-    line(0, y, lineProgress, y);
-    lineProgress += 10;
-    if (lineProgress >= width) {
-      currentLine++;
-      lineProgress = 0;
-    }
-  }
-  
-  // Animate quote movement
+  fill(sunColor);
+  ellipse(sunX, sunY, sunRadius, sunRadius);
+}
+
+function drawText() {
   targetX = mouseX;
-  targetY = height / 1.7; // center vertically
+  targetY = height / 1.5;
 
   quoteX += (targetX - quoteX) * easing;
   quoteY += (targetY - quoteY) * easing;
 
   noStroke();
-  fill(255); 
+  fill(255);
   textAlign(CENTER, CENTER);
-  textFont('Termina'); // Assume Termina is correctly loaded with weight 600
-  textSize(windowWidth / 20); // Make text size responsive to window width
-  
-  let lineHeight = windowWidth / 15; // Adjust line height based on window width
-  
+  textFont('Termina');
+  textSize(windowWidth / 26);  // Further decreased the size to make the text smaller
+  textStyle(BOLD);
+
+  let lineHeight = windowWidth / 14;
   for (let i = 0; i < quoteText.length; i++) {
     text(quoteText[i], quoteX, quoteY + (i - quoteText.length / 2) * lineHeight);
   }
+}
 
-  // Move custom cursor
+function updateCursor() {
   if (cursorDiv) {
     cursorDiv.position(mouseX, mouseY);
   }
@@ -123,4 +158,16 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   timeX = width / 4;
   timeY = height / 4;
+
+  // Recenter the sun on window resize
+  sunX = width / 2;
+  sunY = height / 2;
+
+  // Update sun size proportional to new screen size
+  updateSunSize();
+}
+
+function updateSunSize() {
+  // Set the sun's radius to be 50% of the screen width
+  sunRadius = width * 0.5; // Sun will take up half of the screen's width
 }
